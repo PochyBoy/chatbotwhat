865c9b63b631740bce136cde20855b17979230ae
Julio Kenneth Andersson Mazuelos Vargas <kennethmazuelosvargas.km@gmail.com>
2023-04-29 19:34:15 -0500

    se colocó el token duro y se retiró del env

--- src/controller/whatsapp.controller.js
+++ src/controller/whatsapp.controller.js
@@ -4,7 +4,7 @@ import config from "../config"
 
 export const verifyToken = (req,res) =>{
     try {
-        let accessToken = config.whatsapp
+        let accessToken = 'esteesuntokendewhatperonadaseguro'
         const challenge = req.query['hub.challenge']
         if ( req.query['hub.mode'] == 'subscribe' &&
         req.query['hub.verify_token'] == accessToken) {
